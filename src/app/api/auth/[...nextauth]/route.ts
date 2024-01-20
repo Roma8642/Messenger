9de01d2dd735b0e735c68e3e1c644086cf93feca
Roman <141335182+Roma8642@users.noreply.github.com>
2024-01-20 17:44:52 +0200

    comm

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -51,7 +51,7 @@ export const authOptions: AuthOptions = {
                 return user;
             }
         })
-    ],
+    ],//lalala
     debug: process.env.NODE_ENV === 'development',
     session: {
         strategy: "jwt",
